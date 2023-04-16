d19e191be1bda08d6788da92c328d2d440c0d384
jcarranzav103 <jo.ca.vi@hotmail.com>
2023-04-16 17:25:27 -0600

    Update

--- js/googleApi/placeApi.js
+++ js/googleApi/placeApi.js
@@ -23,40 +23,6 @@ function getPlaceDetails(placeId) {
 
   }
 
-//   function cargarDatos(){
-  
-//     const testimonialCarousel = document.getElementById('idtestimonios');
-//     testimonialCarousel.innerHTML = '';
-
-// for (let i = 0; i < datos.length; i++) {
-//     const testimonialItem = document.createElement('div');
-//     testimonialItem.classList.add('testimonial-item', 'text-center', 'text-white');
-
-//     const img = document.createElement('img');
-//     img.classList.add('img-fluid', 'mx-auto', 'rounded', 'mb-4');
-//     img.src = datos[i].profile_photo_url;
-
-    
-//     const nombre = document.createElement('h4');
-//     nombre.classList.add('text-white', 'mb-0');
-//     nombre.textContent = datos[i].author_name;
-
-//     const hr = document.createElement('hr');
-//     hr.classList.add('mx-auto', 'w-25');
-
-//     const texto = document.createElement('p');
-//     texto.classList.add('fs-5');
-//     texto.textContent = datos[i].texto;
-
-
-//     testimonialItem.appendChild(img);
-//     testimonialItem.appendChild(texto);
-//     testimonialItem.appendChild(hr);
-//     testimonialItem.appendChild(nombre);
-
-//     testimonialCarousel.appendChild(testimonialItem);
-// }
-
 function cargarDatos(){
   
    const img='';
