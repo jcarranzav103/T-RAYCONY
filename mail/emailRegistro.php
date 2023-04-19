a0d380e8864a07db5825fb14200aca4b7c31dbcc
jcarranzav103 <jo.ca.vi@hotmail.com>
2023-04-19 14:49:17 -0600

    Update emailRegistro.php

--- mail/emailRegistro.php
+++ mail/emailRegistro.php
@@ -25,52 +25,11 @@ if(empty($_POST['name'])             ||
    // $academic_degree = strip_tags(htmlspecialchars($_POST['academic_degree']));
    // $gender = strip_tags(htmlspecialchars($_POST['gender']));
  
-   $table = '<table class="table">
-   <tbody>
-      <tr>
-         <td>Nombre:</td>
-         <td>' . $name . '</td>
-      </tr>
-      <tr>
-         <td>Fecha de nacimiento:</td>
-         <td>' . $birthdate . '</td>
-      </tr>
-      <tr>
-         <td>Edad:</td>
-         <td>' . $age . '</td>
-      </tr>
-      <tr>
-         <td>Teléfono:</td>
-         <td>' . $phone . '</td>
-      </tr>
-      <tr>
-         <td>Correo electrónico:</td>
-         <td>' . $email_address . '</td>
-      </tr>
-      <tr>
-         <td>Rango de ingresos:</td>
-         <td>' . $income_range . '</td>
-      </tr>
-      <tr>
-         <td>Grado académico:</td>
-         <td>' . $academic_degree . '</td>
-      </tr>
-      <tr>
-         <td>Género:</td>
-         <td>' . $gender . '</td>
-      </tr>
-      <tr>
-         <td>Mensaje:</td>
-         <td>' . $message . '</td>
-      </tr>
-   </tbody>
-</table>';
-
    
 // Create the email and send the message
 $to = 'registro@rayconycr.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Mensaje del sitio web de :  $name";
-$email_body = "Tu has recibido un mensaje desde la pagina de RayCony CR.\n\nAcá los detalles:\n\n" . $table;
+$email_body = "Tu has recibido un mensaje desde la pagina de RayCony CR.\n\n"."Acá los detalles:\n\nNombre: $name\n\nFecha de cumpleaños: $birthdate\n\nEdad: $age\n\nTeléfono: $phone\n\nCorreo: $email_address\n\nRango de salario: $income_range\n\nGrado academico: $academic_degree\n\nGenero: $gender\n\nMensaje:\n$message";
 $headers = "From: noreply@rayconycr.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";  
 $headers .= "Content-Type: text/html; charset=UTF-8\n"; 
