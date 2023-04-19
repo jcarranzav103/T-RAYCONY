2d14506585fbb37e46a14fe9f49162b953cb29b6
jcarranzav103 <jo.ca.vi@hotmail.com>
2023-04-19 08:55:40 -0600

    Update emailContacto.php

--- mail/emailContacto.php
+++ mail/emailContacto.php
@@ -17,8 +17,8 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
 $to = 'info@rayconycr.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_subject = "Mensaje del sitio web de :  $name";
+$email_body = "Tu has recibido un mensaje desde la pagina de RayCony CR.\n\n"."Acá los detalles:\n\nNombre: $name\n\nCorreo: $email_address\n\nTeléfono: $phone\n\nMensaje:\n$message";
 $headers = "From: noreply@rayconycr.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
