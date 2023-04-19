c3479047fde5b32d99f938beb5702fb2e308be7d
jcarranzav103 <jo.ca.vi@hotmail.com>
2023-04-19 12:17:11 -0600

    update

--- mail/emailRegistro.php
+++ mail/emailRegistro.php
@@ -30,47 +30,6 @@ if(empty($_POST['name'])             ||
 $to = 'registros@rayconycr.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Registro enviado por:  $name";
 $email_body = "Has recibido una solicitud de registro con los siguientes detalles.\n\n";
-$email_body .= '<table class="table table-striped">
-<tbody>
-  <tr>
-    <th>Nombre:</th>
-    <td>'. $name .'</td>
-  </tr>
-  <tr>
-    <th>Fecha de nacimiento:</th>
-    <td>'. $birthdate .'</td>
-  </tr>
-  <tr>
-    <th>Edad:</th>
-    <td>'. $age .'</td>
-  </tr>
-  <tr>
-    <th>Teléfono:</th>
-    <td>'. $phone .'</td>
-  </tr>
-  <tr>
-    <th>Email:</th>
-    <td>'. $email .'</td>
-  </tr>
-  <tr>
-    <th>Rango de ingresos:</th>
-    <td>'. $income_range .'</td>
-  </tr>
-  <tr>
-    <th>Grado académico:</th>
-    <td>'. $academic_degree .'</td>
-  </tr>
-  <tr>
-    <th>Género:</th>
-    <td>'. $gender .'</td>
-  </tr>
-  <tr>
-    <th>Mensaje:</th>
-    <td>'. $message .'</td>
-  </tr>
-</tbody>
-</table>';
-
 $headers .= " From: noreply@rayconycr.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= " Reply-To: $email"; 
 $headers .= " Cc: $email\n"; // Add the user's email address to the "Cc" field to send a copy to them  
