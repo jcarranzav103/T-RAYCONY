f1158461f7ea381e69523bf521092374c76b26ce
jcarranzav103 <jo.ca.vi@hotmail.com>
2023-04-19 07:22:27 -0600

    update

--- mail/emailRegistro.php
+++ mail/emailRegistro.php
@@ -20,7 +20,8 @@ $to = 'registro@rayconycr.com;'; // Add your email address inbetween the '' repl
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@rayconycr.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$headers .= "Reply-To: $email_address"; 
+$headers .= "Cc: $email_address\n"; // Add the user's email address to the "Cc" field to send a copy to them  
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
