2bcc3ba7a0e674b5518dc0b96c1bb41e409363c8
jcarranzav103 <jo.ca.vi@hotmail.com>
2023-04-19 08:38:34 -0600

    Update emailContacto.php

--- mail/emailContacto.php
+++ mail/emailContacto.php
@@ -16,14 +16,12 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 $fila_tabla = '<tr><td>' . $name . '</td><td>'. $email_address .'</td><td>' . $phone . '</td><td>' . $message . '</td></tr>';
 
-
-
    
 // Create the email and send the message
 $to = 'info@rayconycr.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 
 $email_subject = "Website Contact Form:  $name";
-$email_body =;
+$email_body =$message;
 $headers = 'MIME-Version: 1.0' . "\r\n";
 $headers .='Content-type: text/html; charset=iso-8859-1' . "\r\n";
 // $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
@@ -32,5 +30,3 @@ $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
-
-
