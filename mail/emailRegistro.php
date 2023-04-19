d56d135b8d976ed0e335997c2da54465db8d15ba
jcarranzav103 <jo.ca.vi@hotmail.com>
2023-04-19 12:08:17 -0600

    Update emailRegistro.php

--- mail/emailRegistro.php
+++ mail/emailRegistro.php
@@ -1,14 +1,14 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['birthdate'])     ||
-   empty($_POST['age'])     ||
-   empty($_POST['phone'])     ||
-   empty($_POST['email'])     ||
+if(empty($_POST['name'])             ||
+   empty($_POST['birthdate'])        ||
+   empty($_POST['age'])              ||
+   empty($_POST['phone'])            ||
+   empty($_POST['email'])            ||
    empty($_POST['income_range'])     ||
-   empty($_POST['academic_degree'])     ||
-   empty($_POST['gender'])     ||
-   empty($_POST['message'])   ||
+   empty($_POST['academic_degree'])  ||
+   empty($_POST['gender'])           ||
+   empty($_POST['message'])          ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
    echo "No arguments Provided!";
@@ -30,9 +30,47 @@ if(empty($_POST['name'])      ||
 $to = 'registros@rayconycr.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Registro enviado por:  $name";
 $email_body = "Has recibido una solicitud de registro con los siguientes detalles.\n\n";
+$email_body .= '<table class="table table-striped">
+<tbody>
+  <tr>
+    <th>Nombre:</th>
+    <td>'. $name .'</td>
+  </tr>
+  <tr>
+    <th>Fecha de nacimiento:</th>
+    <td>'. $birthdate .'</td>
+  </tr>
+  <tr>
+    <th>Edad:</th>
+    <td>'. $age .'</td>
+  </tr>
+  <tr>
+    <th>Teléfono:</th>
+    <td>'. $phone .'</td>
+  </tr>
+  <tr>
+    <th>Email:</th>
+    <td>'. $email .'</td>
+  </tr>
+  <tr>
+    <th>Rango de ingresos:</th>
+    <td>'. $income_range .'</td>
+  </tr>
+  <tr>
+    <th>Grado académico:</th>
+    <td>'. $academic_degree .'</td>
+  </tr>
+  <tr>
+    <th>Género:</th>
+    <td>'. $gender .'</td>
+  </tr>
+  <tr>
+    <th>Mensaje:</th>
+    <td>'. $message .'</td>
+  </tr>
+</tbody>
+</table>';
 
-$headers = " MIME-Version: 1.0" . "\r\n";
-$headers .= " Content-Type: text/html; charset=UTF-8" . "\r\n";
 $headers .= " From: noreply@rayconycr.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= " Reply-To: $email"; 
 $headers .= " Cc: $email\n"; // Add the user's email address to the "Cc" field to send a copy to them  
