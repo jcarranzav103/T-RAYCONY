10977eb378177cf64bdc09fe88626fe26b65d7d0
jcarranzav103 <jo.ca.vi@hotmail.com>
2023-04-19 07:56:12 -0600

    Update emailContacto.php

--- mail/emailContacto.php
+++ mail/emailContacto.php
@@ -14,21 +14,36 @@ $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
+$fila_tabla = '<tr><td>' . $name . '</td><td>'. $email_address .'</td><td>' . $phone . '</td><td>' . $message . '</td></tr>';
+
    
 // Create the email and send the message
-$to = 'info@rayconycr.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'info@rayconycr.com' . ', '; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to .= $email_address;
 $email_subject = "Website Contact Form:  $name";
-$email_body = "<html><body>";
-$email_body .= "<h2>You have received a new message from your website contact form:</h2>";
-$email_body .= "<p><strong>Name:</strong> $name</p>";
-$email_body .= "<p><strong>Email:</strong> $email_address</p>";
-$email_body .= "<p><strong>Phone:</strong> $phone</p>";
-$email_body .= "<p><strong>Message:</strong> $message</p>";
-$email_body .= "</body></html>";$headers = "From: noreply@rayconycr.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
-$headers .= "Cc: $email_address\n"; // Add the user's email address to the "Cc" field to send a copy to them  
-$headers .= "MIME-Version: 1.0\n";
-$headers .= "Content-Type: text/html; charset=UTF-8\n"; 
+$email_body ='
+<html>
+<head>
+  <title>Mensaje de contacto</title>
+</head>
+<body>
+  <p>¡Ha recivido un mensaje del formulario de contacto!</p>
+  <table>
+    <tr>
+      <th>De</th><th>Correo</th><th>Telefono</th><th>Mensaje</th>
+    </tr>
+    <tr>
+    '.$fila_tabla.'
+    </tr>    
+  </table>
+</body>
+</html>
+';
+$headers = 'MIME-Version: 1.0' . "\r\n";
+$headers .='Content-type: text/html; charset=iso-8859-1' . "\r\n";
+// $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers .= "From: noreply@rayconycr.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
