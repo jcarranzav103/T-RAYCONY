c05dc1f4264c1077eaeec47fc2cddeadcf7c4861
jcarranzav103 <jo.ca.vi@hotmail.com>
2023-04-19 07:33:23 -0600

    Update emailContacto.php

--- mail/emailContacto.php
+++ mail/emailContacto.php
@@ -18,10 +18,17 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = 'info@rayconycr.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@rayconycr.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$email_body = "<html><body>";
+$email_body .= "<h2>You have received a new message from your website contact form:</h2>";
+$email_body .= "<p><strong>Name:</strong> $name</p>";
+$email_body .= "<p><strong>Email:</strong> $email_address</p>";
+$email_body .= "<p><strong>Phone:</strong> $phone</p>";
+$email_body .= "<p><strong>Message:</strong> $message</p>";
+$email_body .= "</body></html>";$headers = "From: noreply@rayconycr.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
-$headers .= "Cc: $email_address\n"; // Add the user's email address to the "Cc" field to send a copy to them   
+$headers .= "Cc: $email_address\n"; // Add the user's email address to the "Cc" field to send a copy to them  
+$headers .= "MIME-Version: 1.0\n";
+$headers .= "Content-Type: text/html; charset=UTF-8\n"; 
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
