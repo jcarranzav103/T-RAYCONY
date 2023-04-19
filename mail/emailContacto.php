df4a242db9aa1fc21c46f1f7be6d4408363da7d7
jcarranzav103 <jo.ca.vi@hotmail.com>
2023-04-19 12:28:47 -0600

    correos

--- mail/emailContacto.php
+++ mail/emailContacto.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'info@rayconycr.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'jcarranza.utn@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Mensaje del sitio web de :  $name";
 $email_body = "Tu has recibido un mensaje desde la pagina de RayCony CR.\n\n"."Acá los detalles:\n\nNombre: $name\n\nCorreo: $email_address\n\nTeléfono: $phone\n\nMensaje:\n$message";
 $headers = "From: noreply@rayconycr.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
